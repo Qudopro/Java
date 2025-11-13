3a8f610c7118aed64c70cb15fdf97e88eb2e10fb
Mac DC <dc@MacBook-Pro-de-Mac.local>
2025-11-13 00:37:12 -0600

    se prueba conexion

--- src/main/java/com/qudopro/ch02/ImprimirMensajes1.java
+++ src/main/java/com/qudopro/ch02/ImprimirMensajes1.java
@@ -2,6 +2,6 @@ package com.qudopro.ch02;
 
 public class ImprimirMensajes1 {
     public static void main(String[] args){
-        System.out.println("Bienvenido a Java!");
+        System.out.println("Bienvenido a Java");
     }
 }
